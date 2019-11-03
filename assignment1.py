a17d177369d1593f641f67189ba74015e3c2c06d
Camillo Rohe <Camillo-Rohe@web.de>
2019-11-03 23:39:30 +0100

    Update assignment1.py
    added bias term. Please check if it runs correctly

--- assignment1.py
+++ assignment1.py
@@ -15,6 +15,9 @@ y = torch.eye(l3)
 # initial weights and biases
 w1 = torch.randn(l2,l1)
 w2 = torch.randn(l3, l2)
+b1 = np.zeros(shape=(l2, 1))           # vector of shape (3, 1)
+b2 = np.zeros(shape=(l3, 1))           # vector of shape (8, 1)
+
 # b0 = torch.randn(1)
 # b1 = torch.randn(1)
 
@@ -24,10 +27,10 @@ for epoch in range(epoches):
     
     # forward propagation
     # z1 = torch.mm(w1, data).add(b0)
-    z1 = torch.mm(w1, data)
+    z1 = torch.mm(w1, data) + b1
     a1 = torch.sigmoid(z1) # activate in layer1 (hidden layer)
     # z2 = torch.mm(w2, a1).add(b1)
-    z2 = torch.mm(w2, a1)
+    z2 = torch.mm(w2, a1) + b2
     a2 = torch.sigmoid(z2) # activate in layer2(output layer)
 
     # loss function
@@ -39,22 +42,31 @@ for epoch in range(epoches):
     da2 = a2.sub(y).mul(2)   # derivative of activate in layer2
     dz2 = torch.mul(da2, sigmoid_derivate(z2)) 
     dw2 = torch.mm(dz2, a1.T).div(m)
+    db2 = (1 / m) * np.sum(dZ2)   # bias term
 
+    
     # layer1 (hidden layer)
     da1 = torch.mm(w2.T, dz2)
     dz1 = torch.mul(da1, sigmoid_derivate(z1))
     dw1 = torch.mm(dz1, data.T).div(m)
+    db1 = (1 / m) * np.sum(dZ1)   # bias term
+
 
     # update weights and biases
     w1 -= alpha*dw1
+    b1 = b1 - learning_rate * db1
+
+    
     w2 -= alpha*dw2
+    b2 = b2 - learning_rate * db2
+
 
     print("epoch: "+ str(epoch) + ", loss: "+ str(loss.item()))
 
 # test
-z1 = torch.mm(w1, data)
+z1 = torch.mm(w1, data) + b1
 a1 = torch.sigmoid(z1)
-z2 = torch.mm(w2, a1)
+z2 = torch.mm(w2, a1) + b2
 a2 = torch.sigmoid(z2) 
 print(a2)
 print(a2.argmax(dim=0)) # print the index of max value in each line
