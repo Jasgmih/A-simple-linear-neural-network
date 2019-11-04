ad3880e3a899bf0c7427e40fe00f862d8af97137
Jasgmih <jingmin.hu@outlook.com>
2019-11-04 19:43:19 +0100

    added biases and tanh function

--- assignment1.py
+++ assignment1.py
@@ -1,33 +1,37 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+
 m = 8  # number of examples
-l1, l2, l3 = 8, 3, 8  # number of neuros in layer0, layer1 and layer2
-alpha = 0.3  # learning rate
+l1, l2, l3 = 8, 3, 8   # number of neuros in layer0, layer1 and layer2
+alpha = 4  # learning rate
 
 # derivate of sigmoid function
 def sigmoid_derivate(x): 
     return torch.mul(torch.sigmoid(x), 1-torch.sigmoid(x))
 
+# derivate of tanh function
+def tanh_derivate(x):
+    return 1-torch.tanh(x)**2
+
 data= torch.eye(l1)
 y = torch.eye(l3)
 
 # initial weights and biases
-w1 = torch.randn(l2,l1)
-w2 = torch.randn(l3, l2)
-# b0 = torch.randn(1)
-# b1 = torch.randn(1)
+w1 = torch.randn(l2,l1)* 0.01
+w2 = torch.randn(l3, l2)* 0.01
+b1 = torch.randn(1)*0.01
+b2 = torch.randn(1)*0.01
 
 losses = []
-epoches = 20000
+epoches = 1000
 for epoch in range(epoches):
     
     # forward propagation
-    # z1 = torch.mm(w1, data).add(b0)
-    z1 = torch.mm(w1, data)
-    a1 = torch.sigmoid(z1) # activate in layer1 (hidden layer)
-    # z2 = torch.mm(w2, a1).add(b1)
-    z2 = torch.mm(w2, a1)
+    z1 = torch.mm(w1, data).add(b1)
+    a1 = torch.tanh(z1) # activate in layer1 (hidden layer)
+
+    z2 = torch.mm(w2, a1).add(b2)
     a2 = torch.sigmoid(z2) # activate in layer2(output layer)
 
     # loss function
@@ -36,35 +40,36 @@ for epoch in range(epoches):
 
     # back propagation
     # layer2 (output layer)
-    da2 = a2.sub(y).mul(2)   # derivative of activate in layer2
-    dz2 = torch.mul(da2, sigmoid_derivate(z2)) 
+    da2 = a2.sub(y).mul(2)   # derivative of a2
+    dz2 = torch.mul(da2, sigmoid_derivate(z2)) # derivative of z2
     dw2 = torch.mm(dz2, a1.T).div(m)
-
+    db2 = torch.sum(dz2).div(m)  
+   
     # layer1 (hidden layer)
     da1 = torch.mm(w2.T, dz2)
-    dz1 = torch.mul(da1, sigmoid_derivate(z1))
+    dz1 = torch.mul(da1, tanh_derivate(z1))
     dw1 = torch.mm(dz1, data.T).div(m)
+    db1 = torch.sum(dz1).div(m)  
 
     # update weights and biases
     w1 -= alpha*dw1
+    b1 -= alpha*db1 
     w2 -= alpha*dw2
+    b2 -= alpha*db2
 
     print("epoch: "+ str(epoch) + ", loss: "+ str(loss.item()))
 
 # test
-z1 = torch.mm(w1, data)
-a1 = torch.sigmoid(z1)
-z2 = torch.mm(w2, a1)
+z1 = torch.mm(w1, data).add(b1)
+a1 = torch.tanh(z1)
+z2 = torch.mm(w2, a1).add(b2)
 a2 = torch.sigmoid(z2) 
 print(a2)
-print(a2.argmax(dim=0)) # print the index of max value in each line
+# print(a2.argmax(dim=0)) # print the index of max value in each line
 
-# plt.xlabel("epoch") 
-# plt.ylabel("loss") 
+plt.xlabel("epoch") 
+plt.ylabel("loss") 
 plt.plot(np.arange(epoches), losses)
 plt.show()
 
 
-"""
-Haven't added bias yet
-"""
